afbb0e1bab327269bcab289e3475f0d1b5590cbb
Iris Martinez <iris.annelise.martinez@outlook.com>
2026-02-19 02:43:54 -0500

    Generate a summary of a provided excerpt of text

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 import torch
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
-
+import sys
+import time
+from transformers import TextStreamer
 
 model = "google/gemma-3n-E4B-it"
 
 
 tokenizer = AutoTokenizer.from_pretrained(model)
-model = AutoModelForCausalLM.from_pretrained(model, device_map='auto')
+model = AutoModelForCausalLM.from_pretrained(model).to("cuda:0")
+
+file = "./text.txt"
+
+text = open(file)
+
 
 if __name__ == "__main__":
-    import sys
-    import time
-    from transformers import TextStreamer
+    prompt = text.read()    
 
-    prompt = " ".join(sys.argv[1:]) if len(sys.argv) > 1 else input("Enter prompt: ")
+    messages = [
+        {"role": "system", "content": "Your goal is to rewrite the provided text in your own words while preserving its main idea and its nuanced meaning.Do not provide any output outside of your rewritten version. Aim to preserve the legnth of the text."},
+        {"role": "user", "content": prompt}
+    ]
     
-    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+    input_ids = tokenizer.apply_chat_template(
+        messages,
+        add_generation_prompt=True, # Essential to start with <|assistant|>
+        return_tensors="pt"
+    ).to(model.device)
     
     streamer = TextStreamer(tokenizer, skip_prompt=True)
-    
-    print(f"\nGenerating response for: '{prompt}'\n")
-    
     start_time = time.time()
-    outputs = model.generate(**inputs, max_new_tokens=200, streamer=streamer)
+    outputs = model.generate(**input_ids, max_new_tokens=2000, streamer=streamer)
     end_time = time.time()
     
-    num_tokens = len(outputs[0]) - len(inputs["input_ids"][0])
+    num_tokens = len(outputs[0]) - len(input_ids["input_ids"][0])
     time_taken = end_time - start_time
     tokens_per_second = num_tokens / time_taken
     
