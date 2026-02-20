ee66b003be9a10d8241170f604a604f593defa73
Iris Martinez <iris.annelise.martinez@outlook.com>
2026-02-20 12:46:58 -0500

    Fix merge conflict

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
+from transformers import PromptDepthAnythingConfig
 import torch
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 import sys
 import time
 from transformers import TextStreamer
+<<<<<<< HEAD
+=======
+from transformers import pipeline
 
-model = "google/gemma-3n-E4B-it"
+model = "Qwen/Qwen3-0.6B"
+# model = "google/gemma-3n-E2B-it"
+#model = "ServiceNow-AI/Apriel-1.5-15b-Thinker"
 
+device = "cuda:0"
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
 
-tokenizer = AutoTokenizer.from_pretrained(model)
-model = AutoModelForCausalLM.from_pretrained(model).to("cuda:0")
-
-file = "./text.txt"
-
-text = open(file)
-
-
-if __name__ == "__main__":
-    prompt = text.read()    
-
-    messages = [
-        {"role": "system", "content": "Your goal is to rewrite the provided text in your own words while preserving its main idea and its nuanced meaning.Do not provide any output outside of your rewritten version. Aim to preserve the legnth of the text."},
-        {"role": "user", "content": prompt}
+file = open("./texts/1984_1.txt")
+pipe= pipeline("text-generation", model=model,device=device)
+input = file.read()
+streamer = TextStreamer(pipe.tokenizer, skip_prompt=True)
+for i in range(1,100):
+    summarize = [
+        {'role':'system', 'content':'Rewrite this passage in your own words and in a similar length and style.'},
+        {'role':'user', 'content':input}
     ]
-    
-    input_ids = tokenizer.apply_chat_template(
-        messages,
-        add_generation_prompt=True, # Essential to start with <|assistant|>
-        return_tensors="pt"
-    ).to(model.device)
-    
-    streamer = TextStreamer(tokenizer, skip_prompt=True)
-    start_time = time.time()
-    outputs = model.generate(**input_ids, max_new_tokens=2000, streamer=streamer)
-    end_time = time.time()
-    
-    num_tokens = len(outputs[0]) - len(input_ids["input_ids"][0])
-    time_taken = end_time - start_time
-    tokens_per_second = num_tokens / time_taken
-    
-    print(f"\n\nStats:\nTokens generated: {num_tokens}\nTime taken: {time_taken:.2f}s\nTokens per second: {tokens_per_second:.2f}")
+    output = pipe(
+        summarize,
+        max_new_tokens=2000,
+        return_full_text=False,
+        do_sample=True,
+        temperature=0.7,
+        tokenizer_encode_kwargs={"enable_thinking": False},
+    )
+    print(f'Iteration number {i}\n {output}')
+    input = output[0]["generated_text"]
